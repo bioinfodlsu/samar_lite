551fde3cd6115187ff5aff3b0dcafff1f48d0ad8
kylesans <kyle_santiago@dlsu.edu.ph>
2022-02-28 14:21:03 +0800

    updated main of reference

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -3,16 +3,15 @@ use std::fs::File;
 use serde::{Serialize};
 use std::path::Path;
 use bio::io::fasta;
-use bio::data_structures::qgram_index;
 use std::collections::HashMap;
 use bv::BitVec;
 use bio::data_structures::rank_select::RankSelect;
 use bio::alphabets::Alphabet;
-use bio::alphabets;
 use bio::data_structures::suffix_array::suffix_array;
-use argparse::{ArgumentParser, StoreTrue, Store};
+use argparse::{ArgumentParser, Store};
+use std::hash::BuildHasherDefault;
+use twox_hash::xxh3::hash64;
 
-// Reference structure
 #[derive(Serialize)]
 struct Ref {
     cat_str: String,
@@ -20,7 +19,7 @@ struct Ref {
 	suff_arry: Vec<usize>,
     rank_select: RankSelect,
 	ref_names: HashMap<u64,String>,
-	hash_table: HashMap<String,(u64,u64)>,
+	hash_table: HashMap<u64, (u64,u64)>,
 	k: usize
 }
 
@@ -28,38 +27,27 @@ fn main() {
 	/*To Do: 
 		Add ArgParse to Input the reference
 	*/
-
-	// Default values for reference file path and k-mer size
 	let mut ref_path = String::from("ref.fasta");
 	let mut k:usize = 5;
-
 	{ // this block limits scope of borrows by ap.refer() method
-		let mut ap = ArgumentParser::new();
-		ap.set_description("Reference Creator for Samar-lite");
-		ap.refer(&mut ref_path).add_argument("Reference", Store,"Reference Fasta File");
-		ap.refer(&mut k).add_argument("kmer", Store,"Kmer for alignment");
-		ap.parse_args_or_exit();
+	let mut ap = ArgumentParser::new();
+	ap.set_description("Reference Creator for Samar-lite");
+	ap.refer(&mut ref_path)
+	.add_argument("Reference", Store,
+	"Reference Fasta File");
+	ap.refer(&mut k)
+	.add_argument("kmer", Store,
+	"Kmer for alignment");
+	ap.parse_args_or_exit();
 	}
-	/*
-		COMMENT FOR ^^:
-		It doesn't have an arguement parser for the
-		path to output of the json file containing
-		the suffix array. Instead the name of the file
-		and its path (./) are directly passed as a string
-		literal on line 122 (serde_json::to_writer() function call)
-	*/
 
 	let path_r = Path::new(&ref_path);
     let reader = fasta::Reader::from_file(path_r).expect("File not found");
-
 	let mut ref_names: HashMap<u64,String> = HashMap::new();
 	let mut cat_str = String::new();
 	let mut rank_bitvec: BitVec<u8> = BitVec::new();
-	
-	let q = 5 as u32; // unused variables q and alph
-	let alph = Alphabet::new(&b"ABCDEFGHIJKLMNOPQRSTUVWXYZ$*abcdefghijklmnopqrstuvwxyz"[..]);
 
-	// Creates Concatenated string and Rank Bit Vector and Rank Names Map
+	//Creates Concatenated string and Rank Bit Vector and Rank Names Map
 	let mut nb_reads = 0;
 
 	for result in reader.records() {
@@ -77,15 +65,19 @@ fn main() {
 		nb_reads += 1;
 	}
 
+	println!("DoNE WITH RANK");
+
 	let suff_arry = suffix_array(&cat_str.as_bytes());
 	
-	// K-mer to Suffix Array Interval Hash Table Construction
+	println!("DONE WITH SA");
+
+	//Kmer to Suffix Array Interval Hash Table Construction
 
 	let n = cat_str.len();
-	let mut hash_table: HashMap<String,(u64,u64)> = HashMap::new();
+	let mut hash_table: HashMap<u64, (u64,u64)> = HashMap::new();
 	let mut prev_str = "";
 	
-	let mut j = "";
+	let mut j = u64::MAX;
 	let mut right: u64 = 0;
 	let mut left: u64 = 0;
 
@@ -95,18 +87,22 @@ fn main() {
 			//println!("{}",cat_str[suff_arry[x]..suff_arry[x]+k-1].to_owned());
 
 			if &cat_str[suff_arry[x]..suff_arry[x]+k] != prev_str{
-				if j != ""{
+				if j != u64::MAX{
 					right = (x - 1) as u64;
-					hash_table.insert(j.to_owned(),(left,right));
+					hash_table.insert(j,(left,right));
 				}
-				j = &cat_str[suff_arry[x]..suff_arry[x]+k];
-				prev_str = &j;
+				j = hash64(&cat_str[suff_arry[x]..suff_arry[x]+k].as_bytes());
+				prev_str = &cat_str[suff_arry[x]..suff_arry[x]+k];
+
+				while hash_table.contains_key(&j){
+					j+=1; 
+				}
 				left = x as u64;
 			}
 		}
 	}
-	hash_table.insert(j.to_owned(),(left,(n-1) as u64));
-	
+	hash_table.insert(j,(left,(n-1) as u64));
+
 	let rank_select = RankSelect::new(rank_bitvec,1); 
 	
 	let test = Ref{
@@ -119,8 +115,12 @@ fn main() {
 		k
 	};
 	
-	serde_json::to_writer(&File::create("data_suffarry.json").unwrap(), &test);
-	// ^ Maybe wrap in a try-catch block for safety since it returns a Result type.
+	let mut file = String::from("ref_index_");
+	file.push_str(&k.to_string());
+	file.push_str(".json");
+	
+	
+	serde_json::to_writer(&File::create(file).unwrap(), &test);
 	
 	println!("Done");
 }
