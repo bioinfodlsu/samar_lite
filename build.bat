309be34446aebffa70f9a5da91d6390186b0f806
Pierre Hernandez <pierrevincenthernandez@gmail.com>
2022-03-17 16:47:16 +0800

    Define commands in .bat file for building the executables and placing them in 'bin' directory

--- build.bat
+++ build.bat
@@ -1,24 +1,25 @@
-all: clean bin_file
-# build_reference build_alignr
-clean: bin
-	@del bin\*.exe
-	@rmdir bin
+@echo off
 
-#build_reference:
-#	@echo +++ BUILDING REFERENCE...
-#	@cd reference && cargo build --release && echo +++ DONE BUILDING REFERENCE
-#	@echo +++
+:: For building the reference
+echo +++ BUILDING REFERENCE...
+cd reference && cargo build --release && echo +++ DONE BUILDING REFERENCE
+cd ..
+echo +++
 
-#build_alignr:
-#	@echo +++ BUILDING ALIGNR...
-#	@cd alignr && cargo build --release && echo +++ DONE BUILDING ALIGNR
-#	@echo +++
+:: For building the alignr
+echo +++ BUILDING ALIGNR...
+cd alignr && cargo build --release && echo +++ DONE BUILDING ALIGNR
+cd ..
+echo +++
 
-bin_file:
-	@echo +++ CREATING BIN FILE...
-	@mkdir bin
-	@echo +++ COPYING REFERENCE EXECUTBLE TO BIN FILE
-	@copy reference\target\release\ref-align.exe bin
-	@echo +++ COPYING ALIGNR EXECUTBLE TO BIN FILE
-	@copy alignr\target\release\alignr.exe bin
-	@echo +++ DONE INITIALIZING BIN FILE
+:: For creating bin directory
+echo +++ CREATING BIN DIRECTORY...
+if exist bin (echo +++ `bin` directory already exists) else (md bin)
+
+:: For placing the executbles in the bin directory
+echo +++ COPYING REFERENCE EXECUTBLE TO BIN DIRECTORY
+if exist bin\*.exe (del bin\*.exe && copy reference\target\release\ref-align.exe bin) else (copy reference\target\release\ref-align.exe bin)
+echo +++ COPYING ALIGNR EXECUTBLE TO BIN DIRECTORY
+copy alignr\target\release\alignr.exe bin
+echo +++ DONE INITIALIZING BIN DIRECTORY
+echo +++
